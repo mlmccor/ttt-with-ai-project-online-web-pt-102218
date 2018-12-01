87c39da860d2ebb5a9f5303847ff2457938eeaea
Matthew McCormack <mmccormack1182@gmail.com>
2018-12-01 17:34:39 +0000

    Done.

--- lib/game.rb
+++ lib/game.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Game
   WIN_COMBINATIONS = [
     [0, 1, 2],
@@ -23,6 +24,28 @@ class Game
   end
 
   def won?
+    WIN_COMBINATIONS.each do |combination|
+      position_1 = self.board.cells[combination[0]]
+      position_2 = self.board.cells[combination[1]]
+      position_3 = self.board.cells[combination[2]]
+      if  (position_1 == "X" && position_2 == "X" && position_3 == "X") || (position_1 == "O" && position_2 == "O" && position_3 == "O")
+        return combination
+      end
+    end
+    false
   end
+  
+  def draw?
+    if self.won? == false
+      true
+    end
+  end
+  
+  def over?
+    binding.pry
+    self.won? == true || (self.draw? == true && self.board.turn_count == 10) ? true : false
+  end
+    
+  
 
 end
